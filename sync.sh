4b5cfc238a5b3d3eb92b868c7c19bfbee12f781a
Stian Håklev <shaklev@gmail.com>
2013-04-02 14:37:42 -0400

    fixing sitemap/sync

--- sync.sh
+++ sync.sh
@@ -1,4 +1,3 @@
 !#/bin/bash
 rsync --delete --stats -avzPe ssh output/blog houshuan@reganmian.net:~/public_html/
-www.google.com',
-wget '/webmasters/tools/ping?sitemap=http://reganmian.net/blog/sitemap.xml'
+wget "http://www.google.com/webmasters/tools/ping?sitemap=http://reganmian.net/blog/sitemap.xml"
