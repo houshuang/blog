6291ffcc20d210e1a60c18a5bc3cd680c548676a
Stian Håklev <shaklev@gmail.com>
2014-10-14 16:08:11 -0400

    adding TOC

--- lib/default.rb
+++ lib/default.rb
@@ -7,6 +7,8 @@ include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::XMLSitemap
 require 'time'
 require 'fast-aleck'
+require 'openssl'
+require 'nanoc-embed'
 
 def grouped_articles
   art = sorted_articles.group_by do |a|
@@ -61,21 +63,22 @@ module PostHelper
     end
   end
 
-def get_post_start(post)
-  content = post.compiled_content
-  if content =~ /<!-- more -->/
-    content = content.partition('<!-- more -->').first
-  else
-    a = content.index("</p>")
-    while content[0..a-1].scan(" ").size < 200 do
-      b = content.index("</p>", a+1)
-      break unless b
-      a = b
+  def get_post_start(post)
+    content = post.compiled_content
+    if content =~ /<!-- more -->/
+      content = content.partition('<!-- more -->').first
+    else
+      a = content.index("</p>")
+      while content[0..a-1].scan(" ").size < 200 do
+        b = content.index("</p>", a+1)
+        break unless b
+        a = b
+      end
+      content = content[0..a-1]
     end
-    content = content[0..a-1]
+    content = content.gsub(/\<div style(.+?)\<\/div\>/m, "")
+    return content + "<div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
   end
-  return content + "<div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
-end
 end
 
 include PostHelper
@@ -86,6 +89,26 @@ class Replacements < Nanoc3::Filter
     content.gsub(/^```(.+?)$/, '```language-\1')  # easier to type ```ruby than ```language-ruby
   end
 end
+
+class MyToc < Nanoc3::Filter
+  identifier :mytoc
+  def run(content, params={})
+    c = 0
+    headers = []
+    tmp = content.gsub(/\<h2\>(.+?)\<\/h2\>/) do |hit|
+      hit = hit[4..-6]
+      headers += [hit]
+      c += 1
+      "<h2 id=#{c-1}>#{hit}</h2>"
+    end
+    out = "<ul>"
+    toc = headers.each_with_index do |header, i|
+      out += "<li><a href=##{i}>#{header}</a></li>"
+    end
+    "<div style=\"float:right;font-size:80%;margin:30px;\">" + out + "</ul></div>" + tmp
+  end
+end
+
 
 class Fast < Nanoc3::Filter
   identifier :fast
